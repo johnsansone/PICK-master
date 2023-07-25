35cba64bcaf09c19c1380877d694bd51a6478ec1
johnsansone <33275540+johnsansone@users.noreply.github.com>
2023-07-25 14:55:01 -0400

    Initial commit

---
+++ src/ui/gui/graph/graph_window.py
@@ -0,0 +1,72 @@
+from PyQt5.QtWidgets import *
+from graph.graph_scene import GraphScene
+from graph.graph_view import GraphView
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from graph.node_scence import Scene
+from graph.node_edge import Edge, EDGE_TYPE_BEZIER
+from graph.node_dto import Node
+from graph.node_connector import Socket
+from graph.timeline_dto import Timeline
+
+
+class GraphWindow(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 800, 600)
+
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.layout)
+
+        self.scene = Scene()
+        # self.grScene = self.scene.grScene
+        self.addNodes()
+
+        # node2 = Node(self.scene, "Node 2")
+        self.view = GraphView(self.scene.grScene, self)
+        self.layout.addWidget(self.view)
+
+        self.setWindowTitle('Graph View')
+        #self.show()
+
+        # self.addDebugContent()
+    def addNodes(self):
+        node1 = Node(self.scene, "Node 1", inputs=[1], outputs=[1])
+        node2 = Node(self.scene, "Node 2", inputs=[1], outputs=[1])
+        node3 = Node(self.scene, "Node 3", inputs=[1], outputs=[1])
+        node1.setPos(-220, 300)
+        node2.setPos(-170, 170)
+        node3.setPos(-130, 20)
+
+        edge1 = Edge(self.scene, node1.outputs[0], node2.inputs[0], edge_type=EDGE_TYPE_BEZIER)
+        edge2 = Edge(self.scene, node2.outputs[0], node3.inputs[0], edge_type=EDGE_TYPE_BEZIER)
+        timeline = Timeline(self.scene)
+
+
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu(self)
+        
+        removeNodeAction = contextMenu.addAction("Add node")
+        connectNode = contextMenu.addAction("Add relationship")
+        deConnectNode = contextMenu.addAction("Delete node")
+        undoChanges = contextMenu.addAction("Delete relationship")
+        redoChanges = contextMenu.addAction("Edit node")
+        settings = contextMenu.addAction("Edit relationship")
+        settings = contextMenu.addAction("Timeline orientation")
+        settings = contextMenu.addAction("Interval units")
+
+        action = contextMenu.exec_(self.mapToParent(event.pos()))
+
+
+    def addDebugContent(self):
+        greenBrush = QBrush(Qt.green)
+        outlinePen = QPen(Qt.black)
+        outlinePen.setWidth(2)
+
+        rect = self.grScene.addRect(-100, -100, 80, 100, outlinePen, greenBrush)
+        rect.setFlag(QGraphicsItem.ItemIsMovable)
---
+++ src/ui/gui/graph/node_connector.py
@@ -0,0 +1,31 @@
+
+from graph.gr_node_connector import GRSocket
+
+LEFT_TOP = 1
+LEFT_BOTTOM = 2
+RIGHT_TOP = 3
+RIGHT_BOTTOM = 4
+
+class Socket():
+    def __init__(self, node,index =0, position=LEFT_TOP):
+        self.node = node
+        self.index = index
+        self.position = position
+
+        self.grSocket = GRSocket(self.node.grNode)
+        self.grSocket.setPos(*self.node.getSocketPosition(index, position))
+        self.grSocket.setPos(*self.node.getSocketPosition(index, position))
+
+        self.edge = None
+
+    def getSocketPosition(self):
+        res = self.node.getSocketPosition(self.index, self.position)
+        return res
+
+
+    def setConnectedEdge(self, edge=None):
+        self.edge = edge
+    def hasEdge(self):
+        return self.edge is not None
+
+
